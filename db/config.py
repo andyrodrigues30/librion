11a660887aab429863090200bd2c1a5bff5456f1
andyrodrigues30 <andrea.melissa.rodrigues@gmail.com>
2026-02-21 14:21:42 +0000

    Ref #1 - add database config

---
+++ db/config.py
@@ -0,0 +1,23 @@
+from pydantic_settings import BaseSettings
+from pathlib import Path
+
+class Settings(BaseSettings):
+    POSTGRES_USER: str
+    POSTGRES_PASSWORD: str
+    POSTGRES_DB: str
+    POSTGRES_HOST: str = "localhost"
+    POSTGRES_PORT: int = 5432
+
+    @property
+    def DATABASE_URL(self) -> str:
+        return (
+            f"postgresql+psycopg2://{self.POSTGRES_USER}:{self.POSTGRES_PASSWORD}@"
+            f"{self.POSTGRES_HOST}:{self.POSTGRES_PORT}/{self.POSTGRES_DB}"
+        )
+
+    class Config:
+        # automatically load from .env file
+        env_file = str(Path(__file__).resolve().parent.parent / ".env")
+
+
+settings = Settings()
