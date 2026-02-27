b4686e9ff79d96505f1b14b31b68ac18ba035c51
andyrodrigues30 <andrea.melissa.rodrigues@gmail.com>
2026-02-27 19:46:18 +0000

    Ref #18 - create navbar component

--- frontend/app/layout.tsx
+++ frontend/app/layout.tsx
@@ -1,12 +1,12 @@
 import type { Metadata } from "next";
 import "./globals.css";
-
+import Navbar from "./_components/Navbar";
 
 export const metadata: Metadata = {
   title: "Librion",
-  description: "An open-source, self-hosted library management system designed for small to medium libraries.",
+  description:
+    "An open-source, self-hosted library management system designed for small to medium libraries.",
 };
-
 
 export default function RootLayout({
   children,
@@ -16,6 +16,8 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body>
+        <Navbar />
+
         {children}
       </body>
     </html>
