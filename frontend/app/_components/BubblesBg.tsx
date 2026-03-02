a78452aec3cfec3a2aaa7457f899378f35463be8
andyrodrigues30 <andrea.melissa.rodrigues@gmail.com>
2026-03-02 19:32:47 +0000

    Ref #23 - add bubbles background with blur

---
+++ frontend/app/_components/BubblesBg.tsx
@@ -0,0 +1,9 @@
+export const BubblesBg = () => {
+  return (
+    <div className="top-0 left-0 w-screen h-screen overflow-hidden absolute z-0">
+      <div className="w-[40rem] h-[20rem] bg-[#003AAB] rounded-full blur-[10rem] absolute top-[-5rem] right-0" />
+      <div className="w-[50rem] h-[30rem] bg-[#6A00AB] rounded-full blur-[20rem] absolute left-[-10rem] translate-y-[75%]" />
+      <div className="w-[50rem] h-[35rem] bg-[#00AB97] rounded-full blur-[30rem] absolute bottom-[-10rem] right-[30rem]" />
+    </div>
+  );
+};
--- frontend/app/_components/grids/RecentlyAddedList.tsx
+++ frontend/app/_components/grids/RecentlyAddedList.tsx
@@ -4,13 +4,13 @@ export const RecentlyAddedList = () => {
   const covers = Array.from({ length: 6 });
 
   return (
-    <div className="lg:flex lg:justify-start lg:items-center text-[#FFF] p-12">
+    <div className="w-screen lg:flex lg:justify-start lg:items-center text-[#FFF] p-12">
       <div className="p-8">
         <h2 className="uppercase text-[4rem]">Recently Added</h2>
         <p>A spotlight of books recently added to the collection.</p>
       </div>
 
-      <div className="flex overflow-x-auto m-4 rounded-md">
+      <div className="flex overflow-x-auto m-4">
         {covers.map((_, index) => {
           return <BookCover key={index} />;
         })}
