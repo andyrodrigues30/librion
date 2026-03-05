265f79a057005d40701a3da8e54cbbf32592ec87
andyrodrigues30 <andrea.melissa.rodrigues@gmail.com>
2026-03-05 13:58:44 +0000

    Ref #16 - fix searchbar sizing

--- frontend/app/_views/HomeView.tsx
+++ frontend/app/_views/HomeView.tsx
@@ -1,9 +1,19 @@
+"use client";
+
 import { RecentlyAddedList } from "@/app/_components/grids/RecentlyAddedList";
+import { Searchbar } from "../_components/inputs/field/Searchbar";
 
 export const Homeview = () => {
-    return (
-        <div>
-            <RecentlyAddedList />
-        </div>
-    )
-}
+  return (
+    <div>
+      <RecentlyAddedList />
+
+      <div className="flex justify-center items-center">
+        <Searchbar
+          placeholder="Enter title, author, genre"
+          action={() => console.log("Searching...")}
+        />
+      </div>
+    </div>
+  );
+};
