0bcba0ad3c474ebe11d74e992facee4ec636d6f2
andyrodrigues30 <andrea.melissa.rodrigues@gmail.com>
2026-03-10 17:42:37 +0000

    Ref #15 - add book grid

--- frontend/app/_components/BookCover.tsx
+++ frontend/app/_components/BookCover.tsx
@@ -4,6 +4,7 @@ import { BookCoverProps } from "../_types/BookCard";
 
 export const BookCover = ({width, height}: BookCoverProps) => {
   return (
+    // TODO: make cover clickable
     <div
         style={{ width: `${width}px`, height: `${height}px` }}
         className=" m-2 bg-[#202020] rounded-lg flex-shrink-0"
