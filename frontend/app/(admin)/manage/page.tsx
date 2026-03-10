8a9a399c83e7643798c61952b2473124234bf611
andyrodrigues30 <andrea.melissa.rodrigues@gmail.com>
2026-03-10 17:25:57 +0000

    Ref #25 - create manage view

--- frontend/app/(admin)/manage/page.tsx
+++ frontend/app/(admin)/manage/page.tsx
@@ -1,8 +1,9 @@
+import { ManageView } from "@/app/_views/ManageView";
 
 export default function Manage() {
   return (
-    <div className="">
-      
-    </div>
+    <>
+      <ManageView />
+    </>
   );
 }
